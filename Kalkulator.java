ba6b34bf8b46241751e3d9b25da58ed10a02ce0b
Lananggalih <32382784+Lananggalih@users.noreply.github.com>
2018-11-02 13:59:21 +0700

    Add files via upload

---
+++ Kalkulator.java
@@ -0,0 +1,13 @@
+public class Kalkulator
+{
+    public float plus(float a,float b)
+    {
+        return a+b;
+    }
+    public float minus(float a ,float b)
+    {
+        return a-b;
+    }
+    public float multiply(float a,float b)
+    public float division(float a,float b)
+}
