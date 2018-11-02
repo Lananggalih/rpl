fa0d2ee1ad5368415c79c2c92f4daa7bd591e2de
ADRIAN PACELI <i16046@ftis>
2018-11-02 14:45:09 +0700

    apa

---
+++ Apa/Kalkulator.java
@@ -0,0 +1,23 @@
+public class Kalkulator
+{
+    public float plus(float a,float b){
+        return a+b;
+    }
+    public float minus(float a ,float b){
+        return a-b;
+
+    }
+    public float multiply(float a,float b){
+        return a*b;
+    }
+    public float division(float a,float b){
+        return a/b;
+    }
+	//mang lanas nub
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		float a = sc.nextDouble();
+		float b = sc.nextDouble();
+		System.out.println(plus.(a,b));
+	}
+}
