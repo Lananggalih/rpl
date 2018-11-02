a3b7923d9cc99de8cdff2717e95cc8de76c00dc3
Lananggalih <32382784+Lananggalih@users.noreply.github.com>
2018-11-02 14:20:55 +0700

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,9 +1,10 @@
 public class Kalkulator
 {
     public float plus(float a,float b){
-
+        return a+b;
     }
     public float minus(float a ,float b){
+        return a-b;
 
     }
     public float multiply(float a,float b){
